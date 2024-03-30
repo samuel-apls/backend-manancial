02658ffed299d6ce1ed43df846e0df84b4670c0a
UrielBatista <ur.sabatista@gmail.com>
2024-03-30 11:01:38 -0300

    ✨ Split endpoint of request to get non-valid and valid members

--- routes/MemberRoutes.js
+++ routes/MemberRoutes.js
@@ -13,6 +13,7 @@ router.put("/qualificationsMember", middlewareAuthentication.checkAdminPermition
 
 router.get("/getMember", middlewareAuthentication.checkAnyPermitionsRoles, MemberController.requestManancialMember)
 router.get("/getAllMembers", middlewareAuthentication.checkAnyPermitionsRoles, MemberController.requestAllManancialMember)
+router.get("/getAllNonValidMembers", middlewareAuthentication.checkAnyPermitionsRoles, MemberController.requestNonValidMembers)
 router.get("/getAllMembersWithQualifications", middlewareAuthentication.checkAdminPermitionRole, MemberController.requestMemberWithQualifications)
 
 export default router;
